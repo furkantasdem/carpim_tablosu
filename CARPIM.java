ca705fc03aa566e861609ee16aee892d28db2d77
furkan taşdemir <118552824+tekelfurkan@users.noreply.github.com>
2022-11-19 02:30:50 +0300

    kullanıcı girişi ile çarpım tablosu
    canım sıkıldı yaptım java;

---
+++ CARPIM.java
@@ -0,0 +1,41 @@
+package days.day1;
+
+
+import java.util.Scanner;
+
+public class CARPIM {
+
+    public static void main(String[] args) {
+
+        Scanner  console = new Scanner(System.in);
+        int num;
+
+        System.out.print("Enter any positive integer: ");
+        num = console.nextInt();
+
+        System.out.println("Multiplication Table of " + num);
+
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(num +" x " + i + " = " + (num*i) );
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
